71acaa7e1f91cb30cf106349d47aa0635a8a8d59
khushwant Dafade <awsmkhush@gmail.com>
2022-09-17 19:09:00 +0530

    khushwant ganesh again

--- MiniProject/src/com/ecommerce/miniproject/sqldata/Ganesh.java
+++ MiniProject/src/com/ecommerce/miniproject/sqldata/Ganesh.java
@@ -3,7 +3,7 @@ package com.ecommerce.miniproject.sqldata;
 public class Ganesh {
 
 	public static void main(String[] args) {
-		System.out.println("Ganesh");
+		System.out.println("Hello from Ganesh Raut....");
 
 	}
 
