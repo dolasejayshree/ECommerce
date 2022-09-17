5aa02050e4e1c067f132c70cf1ccfb6ae6ac695a
khushwant Dafade <awsmkhush@gmail.com>
2022-09-17 19:01:53 +0530

    first commit Ganesh

--- MiniProject/src/com/ecommerce/miniproject/sqldata/ConnectionTest.java
+++ MiniProject/src/com/ecommerce/miniproject/sqldata/ConnectionTest.java
@@ -9,7 +9,7 @@ public class ConnectionTest {
 	public Connection getConnectionDetails() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/warehouse","root","Jayshree@123");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/warehouse","root","8390309081");
 			
 		}catch(Exception e) {
 			e.printStackTrace();
---
+++ MiniProject/src/com/ecommerce/miniproject/sqldata/Ganesh.java
@@ -0,0 +1,10 @@
+package com.ecommerce.miniproject.sqldata;
+
+public class Ganesh {
+
+	public static void main(String[] args) {
+		System.out.println("Ganesh");
+
+	}
+
+}
